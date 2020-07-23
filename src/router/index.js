c96847eb20dd766cc73b764ddd46536effca8608
liyao <admin@>
2020-07-23 15:42:57 +0800

    答题游戏主界面

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import MainMenu from '../views/MainMenu.vue'
-
+import GameController from '../views/GameController.vue'
 
 Vue.use(VueRouter)
 
@@ -10,6 +10,16 @@ const routes = [
         name: 'home',
         path: '/',
         component: MainMenu
+    }, {
+        name: 'quiz',
+        path: '/quiz',
+        component: GameController,
+        props: (route) => ({
+            number: route.query.number,
+            difficulty: route.query.difficulty,
+            category: route.query.category,
+            type: route.query.type
+        })
     }
 ]
 
